ff8b6709cbe566e0717e1de74ca6fb12adfe5076
jweihsz <1989428128@qq.com>
2017-05-18 08:35:37 +0800

    add chapper  2

--- you_notknow_javascript/code.js
+++ you_notknow_javascript/code.js
@@ -1,12 +1,37 @@
 
 
-var a = 10;
 
-var a;
+var obj0 = {
+	
+	
+	q:2,
+	
+};
+var obj1 = {
+	
+	
+	f:2,
+	
+};
+
+function test(obj){
+	
+	with(obj){
+		
+		q = 3;	
+	}
+}
+
+test(obj0);
+console.log(obj0.q); //3
+test(obj1);  
+console.log(obj1.q);/*undefined*/
+console.log(q); /*3,q被泄露成全局变量了*/
 
 
 
-console.log("this is a test" + a);
+
+
 
 
 
